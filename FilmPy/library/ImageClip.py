8e321cac514c1f649e924886b2e3b695dca601ae
David Orkin <dorkin@arcthrift.com>
2024-11-18 11:40:27 -0700

    Renamed the input parameter frame_data to video_frames to be more explicit. The function takes a list of frames, not a single frame. Also improved the wording in the ValueError raised when bad inputs are received.

--- FilmPy/library/ImageClip.py
+++ FilmPy/library/ImageClip.py
@@ -8,14 +8,14 @@ class ImageClip(Clip):
     An image clip is a clip comprised of a single image
     """
     def __init__(self,
-                 image_path:str=None,
-                 frame_data:[]=None,
+                 image_path:str=None,  # A path to an image file
+                 video_frames:[]=None, # An array of video frames that comprise this Image
                  start_time:float=0,
                  end_time=None,
                  video_fps=None):
         """
         :param image_path: Path to an image file
-        :param frame_data: Image Data (e.g. a single video frame)
+        :param video_frames: Image Data (e.g. a single video frame)
         :param start_time: Start of time of the clip in seconds
         :param end_time: End time of the clip in seconds
         :param video_fps:
@@ -23,22 +23,25 @@ class ImageClip(Clip):
         :raises:
             ValueError
         """
-        # Make sure we have some data to work with
-        if (image_path is None) and (frame_data is None):
+        # Make sure we have some either an image file path or the video frames themselves
+        if (image_path is None) and (video_frames is None):
             msg = (f"Invalid input received. "
-                   f"Either {classname(self)}.image_path or {classname(self)}.frame_data must be provided.")
+                   f"{classname(self)}: Either image_path or video_frames must be provided.")
             raise ValueError(msg)
 
-        if image_path and frame_data:
+        # Make sure we do not have potentially conflicting image data
+        if image_path and video_frames:
             msg = (f"Conflicting input received. "
                    f"Either provide {classname(self)}.image_path or {classname(self)}.frame_data, not both.")
             raise ValueError(msg)
 
         # We were given the image frame
-        if frame_data and (image_path is None):
-            self._video_frames = [frame_data]
-        elif image_path and (frame_data is None):
-                with open(image_path, 'r') as image_file:
-                    self._video_frames = [numpy.asarray(image_file.read())]
+        if video_frames and (image_path is None):
+            self._video_frames = [video_frames]
+
+        # We were given a path to an image file
+        elif image_path and (video_frames is None):
+            with open(image_path, 'rb') as image_file:
+                self._video_frames = [numpy.asarray(image_file.read())]
 
         super().__init__(start_time=start_time, end_time=end_time, video_fps=video_fps, include_audio=False)
