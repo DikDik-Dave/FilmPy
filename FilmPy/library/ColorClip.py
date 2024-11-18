c81b43b21e9b6dd5ecb2d0f49f1c56a8e432f9db
David Orkin <dorkin@arcthrift.com>
2024-11-18 10:54:11 -0700

    Changed the base class from Clip to ImageClip.

--- FilmPy/library/ColorClip.py
+++ FilmPy/library/ColorClip.py
@@ -1,7 +1,7 @@
-from FilmPy.library import Clip
+from FilmPy.library.ImageClip import ImageClip
 import numpy
 
-class ColorClip(Clip):
+class ColorClip(ImageClip):
     def __init__(self, rgb_color, width, height, video_fps, end_time):
         """
         Video Clip of just a single color
@@ -11,12 +11,9 @@ class ColorClip(Clip):
         :param height: Frame Height
         :param end_time: End Time (Duration) of the clip in seconds
         """
-        # Instantiate Clip
-        super().__init__(frame_width=width, frame_height=height,start_time=0,
-                         end_time=end_time, video_fps=video_fps, write_audio=False)
-
-        # Create the frame
+        # Instantiate ImageClip
         self._frame = numpy.tile(rgb_color, width * height).reshape(height, width, 3).astype('uint8')
+        super().__init__(frame_data=self._frame, end_time=end_time, video_fps=video_fps)
 
     def get_video_frames(self):
         """
@@ -24,17 +21,17 @@ class ColorClip(Clip):
         :return:
         """
         # Calculate the number of frames we need to generate
-        number_frames = int(self.video_fps * self._end_time)
+        number_frames = int(self.video_fps * self._clip_end)
 
         # If we already retrieved the frames, just return them
-        if self._video_frames:
-            return self._video_frames
+        if self._clip_frames:
+            return self._clip_frames
 
         # Create all the frames
         for _ in range(number_frames):
-            self._video_frames.append(self._frame)
+            self._clip_frames.append(self._frame)
 
         # Return the video frames
-        return self._video_frames
+        return self._clip_frames
 
 
