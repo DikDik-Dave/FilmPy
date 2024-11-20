d146d0ad64c570f8d4b456d43d2b580332c75647
David Orkin <dorkin@arcthrift.com>
2024-11-20 15:48:55 -0700

    Renamed from VideoFileClip to VideoClip to be consistent with the approach used with ImageClip. VideoClip can now be instantiated with frame data.

--- FilmPy/library/VideoClip.py
+++ FilmPy/library/VideoClip.py
@@ -7,36 +7,70 @@ from FilmPy.library.constants import FFMPEG_BINARY
 from FilmPy.library import Clip
 
 
-class VideoFileClip(Clip):
+class VideoClip(Clip):
     """
     Video Clip from a file
     """
-    def __init__(self, video_file_path, start_time=0, end_time=None, include_audio=True):
+    def __init__(self, video_path=None,
+                 frames=None,
+                 start_time=0,
+                 end_time=None,
+                 include_audio=True):
         """
-        Initialize a Video File Clip
+        Initialize a VideoClip from either a file or via direct frame data
 
-        :param video_file_path: path to the video file associated with this clip
+        :param video_path: path to the video file associated with this clip
         :param start_time: When does the clip start
         :param end_time: When does the clip end
         :param include_audio: Should the audio be written for this clip
         """
+        # Check to make sure we did not recieve potentially conflicting inputs
+        if frames and video_path:
+            msg = (f" {type(self).__name__} - Conflicting input received. "
+                   f"Either provide frames or video_path, not both.")
+            raise ValueError(msg)
+        elif (not frames) and (not video_path):
+            msg = (f" {type(self).__name__} - Invalid input received. "
+                   f"Either provide frames or video_path.")
+            raise ValueError(msg)
+
         # Initialize Clip, we will set the frame_width, frame_height, and video_fps values
         # after we have processed the metadata for this clip
-        super().__init__(frame_width=None, frame_height=None, start_time=start_time,
-                         end_time=end_time, video_fps=None, include_audio=include_audio)
+        super().__init__(width=None, height=None, start_time=start_time,
+                         end_time=end_time, video_fps=None, include_audio=include_audio,
+                         frames=frames)
 
+        # If we have a video
+        if video_path:
+            self._set_video_info(video_path, end_time)
+
+
+    ###################
+    # Private Methods #
+    ###################
+    def _set_video_info(self, video_path, end_time):
+        """
+        Set the video information about video files
+        :param video_path: Path to the video
+        :param end_time: End time of the clip
+        """
         # Call ffprobe to get information about the file
         ffprobe = FFmpeg(executable="ffprobe").input(
-            video_file_path,
+            video_path,
             print_format="json",  # ffprobe will output the results in JSON format
             show_streams=None,
         )
 
-        ffmpeg_info_command = [FFMPEG_BINARY, "-hide_banner", "-i", video_file_path]
+        self._media = json.loads(ffprobe.execute())
+        self._video_info = self._media['streams'][0]
+        self._audio_stream = self._media['streams'][1]
+
+        # Call ffmpeg to get additional information about the file
+        ffmpeg_info_command = [FFMPEG_BINARY, "-hide_banner", "-i", video_path]
         completed_process = subprocess.run(ffmpeg_info_command, capture_output=True)
         for line in completed_process.stderr.splitlines()[1:]:
             if b'Duration: ' in line:
-                duration, start, bit_rate = line.split( b',')
+                duration, start, bit_rate = line.split(b',')
 
                 # Process the duration
                 hours, minutes, seconds = duration.strip(b'Duration: ').split(b':')
@@ -50,20 +84,18 @@ class VideoFileClip(Clip):
                 # Set the video_fps attribute
                 value_end = line.find(b' fps')
                 value_start = line[:value_end].rfind(b', ')
-                self.video_fps = float(line[value_start+2:value_end])
+                self._video_info['fps'] = float(line[value_start + 2:value_end])
 
                 # Set the video_tbr attribute
                 value_end = line.find(b' tbr,')
                 value_start = line[:value_end].rfind(b', ')
-                self.video_tbr = float(line[value_start+2:value_end])
+                self._video_info['tbr'] = float(line[value_start + 2:value_end])
             elif (b'Stream #0' in line) and (b'Audio:' in line):
                 # print(line)
                 pass
 
-        self._media = json.loads(ffprobe.execute())
-        self._video_info = self._media['streams'][0]
-        self._audio_stream = self._media['streams'][1]
-        self._video_file_path = video_file_path
+
+        self._video_file_path = video_path
 
         # Set video stream attributes
         self._video_info['duration'] = float(self._video_info['duration'])
@@ -75,6 +107,10 @@ class VideoFileClip(Clip):
         if not end_time:
             self._end_time = self.video_duration
 
+
+    ##################
+    # Public Methods #
+    ##################
     def get_audio_data(self,
                        file_name,
                        fps=44100,
@@ -118,8 +154,8 @@ class VideoFileClip(Clip):
         """
 
         # If we already loaded the frames, just return them
-        if self._clip_frames:
-            return self._clip_frames
+        if self._video_frames:
+            return self._video_frames
 
         # Call ffmpeg and pipe it's output
         command = [FFMPEG_BINARY,
@@ -141,6 +177,6 @@ class VideoFileClip(Clip):
                                                                     3))
 
             # Store the frame in our frames list
-            self._clip_frames.append(frame)
+            self._video_frames.append(frame)
 
-        return self._clip_frames
+        return self._video_frames
