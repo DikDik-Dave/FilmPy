993643a5b678fdc4de709185f180e48fc46ce88c
David Orkin <dorkin@arcthrift.com>
2024-12-03 21:34:08 -0700

    Removed commented out code. Corrected the doc string.

--- FilmPy/clips/ColorClip.py
+++ FilmPy/clips/ColorClip.py
@@ -2,36 +2,28 @@ from FilmPy.clips.ImageClip import ImageClip
 import numpy
 
 class ColorClip(ImageClip):
-    def __init__(self, rgb_color, clip_width, clip_height, video_fps, clip_end_time):
+    def __init__(self,
+                 size:tuple,
+                 color=(57,255,20),
+                 **kwargs):
         """
-        Video Clip of just a single color
+        Clip containing a single color
 
-        :param rgb_color: (R,G,B) tuple of the color to be displayed
-        :param clip_width: Frame width
-        :param clip_height: Frame Height
-        :param clip_end_time: End Time (Duration) of the clip in seconds
+        :param size: (width, height) of the clip
+        :param color: Color to make the clip, defaults to a neon green
+        :param kwargs: Keyword arguments to pass on to ImageClip & BaseClip
+
+        :raises ValueError: When size is not a tuple or list
         """
+        # Ensure size is a list or a tuple
+        if not isinstance(size, (tuple, list)):
+            raise ValueError(f"size parameter must be an a tuple or a list")
+
+        # Create a frame of the appropriate size
+        frame = numpy.tile(color, size[0] * size[1]).reshape(size[1], size[0], 3).astype('uint8')
+
         # Instantiate ImageClip
-        self._frame = numpy.tile(rgb_color, clip_width * clip_height).reshape(clip_height, clip_width, 3).astype('uint8')
-        super().__init__(video_frames=self._frame, clip_end_time=clip_end_time, video_fps=video_fps)
-
-    def get_video_frames(self):
-        """
-        Get the video frames for this clip
-        :return:
-        """
-        # Calculate the number of frames we need to generate
-        number_frames = int(self.video_fps * self._clip_end)
-
-        # If we already retrieved the frames, just return them
-        if self._clip_frames:
-            return self._clip_frames
-
-        # Create all the frames
-        for _ in range(number_frames):
-            self._clip_frames.append(self._frame)
-
-        # Return the video frames
-        return self._clip_frames
-
-
+        super().__init__(video_frames=[frame],
+                         clip_width=size[0],
+                         clip_height=size[1],
+                         **kwargs)
