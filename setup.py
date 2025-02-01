422d9017e6d8aebd384cd0693706e2646159c823
David Alan Gardener <contact@dikdik.us>
2025-02-01 16:56:47 -0700

    Bumped the version to 25.2.0 with the assumption we dont make the first release until Q2.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
     name='FilmPy',
-    version='25.1.0.rc0',
+    version='25.2.0.rc0',
     packages=find_packages(include=['FilmPy', 'FilmPy.*'])
 )
