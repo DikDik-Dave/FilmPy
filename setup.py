e4d1c0d050ddab3c85505f2181c29cf55dccce06
David Alan Gardener <contact@dikdik.us>
2025-02-02 20:14:05 -0700

    Added package_data (FilmPy.assets)

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name='FilmPy',
     version='25.2.0.rc0',
-    packages=find_packages(include=['FilmPy', 'FilmPy.*'])
+    packages=find_packages(include=['FilmPy', 'FilmPy.*']),
+    package_data = {"FilmPy.assets": ["**/*"]}
 )
