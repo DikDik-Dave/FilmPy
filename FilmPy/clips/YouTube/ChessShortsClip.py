aa2eb0ac6e62e61f9437b62a2a25bae348a9347e
David Alan Gardener <contact@dikdik.us>
2025-02-04 16:17:27 -0700

    Changed from video_end_time being a default of 3 minutes to being calculated to be 3 minutes or less based on number of moves.

--- FilmPy/clips/YouTube/ChessShortsClip.py
+++ FilmPy/clips/YouTube/ChessShortsClip.py
@@ -8,11 +8,10 @@ class ChessShortsClip(ChessClip):
 
     def __init__(self,
                  chess_board_orientation=Chess.WHITE.value,
-                 chess_move_duration=3,
+                 chess_move_duration=None,
                  chesspy_game=None,
                  file_path=None,
                  clip_size=(1080, 1920),
-                 video_end_time=180,
                  **kwargs):
         """
         Initialize ChessClip object
@@ -20,6 +19,21 @@ class ChessShortsClip(ChessClip):
         :param file_path: Path to a pgn chess file
         :param clip_size: Width and height of the clip. Width of the clip
         """
+
+        # Move duration was not supplied, default to 2 seconds
+        if not chess_move_duration:
+            chess_move_duration = 2
+
+        # If there are over 180, adjust the move duration
+        number_moves = len(chesspy_game.moves)
+
+        if number_moves > 175:
+            chess_move_duration = .75
+        elif number_moves >= 85:
+                chess_move_duration = 1
+
+        # Calculate the video's length, Add a "1 move" padding to the clips duration
+        video_end_time = chess_move_duration * (number_moves+1)
 
         # Initialize the ChessClip
         super().__init__(chess_board_orientation=chess_board_orientation,
@@ -29,4 +43,3 @@ class ChessShortsClip(ChessClip):
                          file_path=file_path,
                          video_end_time=video_end_time,
                          **kwargs)
-
